07e23d4de6c0bf8161df38bd8c95cae2c2991121
Jenish Rudani <jrudani1999@gmail.com>
2022-04-15 11:00:00 -0700

    added changes

--- 1.py
+++ 1.py
@@ -1,13 +1,15 @@
-from statistics import mode
 import tensorflow as tf
 import numpy as np
 from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.layers import Dense
 
 
+print("\n\nInstalled Tensorflow Version : {}\n\n".format(tf.__version__))
+
 myNNLayer = Dense(units=1, input_shape=[1])
 
 model = Sequential([myNNLayer])
+print("\n\n")
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
@@ -17,5 +19,5 @@ model.fit(xs, ys, epochs=500)
 
 inputX = 10.0
 outputY = model.predict([inputX])
-print(" [Optimizer: 'Stochastic Gradient Descent', Loss Funtion: 'Mean Squared Error'] ** Calculated Weights based on model training: ' {} '".format(myNNLayer.get_weights()))
-print("For input X: '{}', My Model Predicted Y: '{}'".format(inputX, outputY))
+print("\n\n [Optimizer: 'Stochastic Gradient Descent', Loss Function: 'Mean Squared Error'] ** Calculated Weights based on model training: ' {} '".format(myNNLayer.get_weights()))
+print("\n\nFor input X: '{}', My Model Predicted Y: '{}'\n\n".format(inputX, outputY))
