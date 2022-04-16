9ee7d6edb3f5c7b0d6b3d40aca63e169405c202c
Jenish-Rudani <jrudani1999@gmail.com>
2022-04-15 23:49:46 -0700

    added chater two file

---
+++ 2.py
@@ -0,0 +1,9 @@
+from ast import For
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import Dense
+
+
+print("\n\nNum GPUs Available: {}\n\n".format(tf.config.list_physical_devices('GPU')))
+
